04882e1cc3a94ce36ddbae02b1f0d965b8d599fd
yichien <kevinyichienlee@gmail.com>
2024-02-15 01:31:32 +0900

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const singup = async (req, res, next) => {
   console.log("req.body", req.body);
@@ -11,6 +12,39 @@ export const singup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  console.log("req.body", req.body);
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "wrong credentials"));
+    }
+    const accessToken = jwt.sign(
+      { id: validUser._id },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    res
+      .cookie("jwt", accessToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "None",
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { singup } from "../controllers/auth.controller.js";
+import { signin, singup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", singup);
+
+router.post("/signin", signin);
 
 export default router;
