9c8982411d904f5ae4a38a5237b53495f8b0f32a
yichien <kevinyichienlee@gmail.com>
2024-02-22 02:37:20 +0800

    create update user api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 dotenv.config();
@@ -14,6 +15,9 @@ mongoose
 
 const app = express();
 app.use(express.json());
+
+// middleware for cookies
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server Listening on port 3000!");
